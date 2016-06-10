9e449ba63bec59fff994baff5db442cbfda2984f
jgretz <jgretz@truefitsolutions.com>
2016-06-10 13:05:50 -0400

    add eslint and background color changing

--- src/host.js
+++ src/host.js
@@ -1,36 +1,5 @@
 import React, { Component } from 'react';
-import { AppRegistry, StyleSheet, Text, View, Platform } from 'react-native';
-
-export default class ToDo extends Component {
-  renderGetStartedText() {
-    switch (Platform.OS) {
-      case "ios":
-        return "IOS";
-
-      case "android":
-        return "Android";
-
-      default:
-        return "Who are you?"
-    }
-  }
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          {this.renderGetStartedText()}
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
+import { StyleSheet, Text, View, Platform } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
@@ -50,3 +19,53 @@ const styles = StyleSheet.create({
     marginBottom: 5,
   },
 });
+
+export default class ToDo extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      colors: ['#ff0000', '#00ff00', '#0000ff'],
+      bgColorIndex: 0,
+    };
+
+    setInterval(() => {
+      let newIndex = this.state.bgColorIndex + 1;
+      if (newIndex >= this.state.colors.length) {
+        newIndex = 0;
+      }
+
+      this.setState({ bgColorIndex: newIndex });
+    }, 1000);
+  }
+  renderGetStartedText() {
+    switch (Platform.OS) {
+      case 'ios':
+        return 'IOS';
+
+      case 'android':
+        return 'Android';
+
+      default:
+        return 'Who are you?';
+    }
+  }
+
+  render() {
+    const bgColor = { backgroundColor: this.state.colors[this.state.bgColorIndex] };
+
+    return (
+      <View style={[styles.container, bgColor]}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          {this.renderGetStartedText()}
+        </Text>
+        <Text style={styles.instructions}>
+          Shake or press menu button for dev menu
+        </Text>
+      </View>
+    );
+  }
+}
