0d200f7c164cf401f6d8a3a9335a2890675ff85e
jgretz <jgretz@truefitsolutions.com>
2016-10-20 19:40:53 -0500

    remove "old"

--- index.ios.js
+++
@@ -1,4 +0,0 @@
-import Host from './src/host';
-import { AppRegistry } from 'react-native';
-
-AppRegistry.registerComponent('ToDo', () => Host);
--- src/controls/index.js
+++
@@ -1,4 +0,0 @@
-export * from './button';
-export * from './input';
-
-export * from './itemlist';
